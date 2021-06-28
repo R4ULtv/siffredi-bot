081c6b27950e9417a5f6d88d967a521f9c82f3d5
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2021-06-28 23:48:53 +0200

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,19 @@
+import discord
+
+
+page1 = discord.Embed(
+    title=":grey_exclamation: This is the list of my funny commands", 
+    description="Use the buttons below to navigate between fun pages.", 
+    colour=discord.Colour.purple())
+page2 = discord.Embed(
+    title="Funny commands #1", 
+    description='`nibba`\n `boomer`\n `mino`\n `ippo`\n `clown`\n `scrofa`\n `down`\n `terrone`\n `helo`\n `mauro`\n',
+    colour=discord.Colour.purple())
+page3 = discord.Embed(
+    title="Funny commands #2", 
+    description='`dio`\n `bit`\n `boss`\n `defendi`\n `gay`\n `kit`\n `cagna`\n `napoli`\n `suora`\n`faggot`', 
+    colour=discord.Colour.purple())
+page4 = discord.Embed(
+    title="Funny commands #3", 
+    description='`fabrizio`\n`cvicev`', 
+    colour=discord.Colour.purple())
